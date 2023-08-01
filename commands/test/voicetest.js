9916ece8fba69e1b3d4709d609a5d2769585aba2
Raymond Wong <raymondhohinwong@gmail.com>
2023-08-01 13:18:21 +0100

    Added audio player that plays mp3 from resources

--- commands/test/voicetest.js
+++ commands/test/voicetest.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, ChannelType } = require("discord.js");
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, AudioPlayer, VoiceConnectionStatus, NoSubscriberBehavior, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const wait = require('node:timers/promises').setTimeout;
+const fs = require('node:fs');
+const path = require('node:path')
 
 module.exports = {
     category: 'test',
@@ -26,11 +28,37 @@ module.exports = {
         console.log('adapterCreator: ' + channel.guild.voiceAdapterCreator);
         */
 
+        const audioPlayer = new AudioPlayer({
+            behaviors: {
+                noSubscribers: NoSubscriberBehavior.Pause,
+            },
+        });
+
+        /*
+        const foldersPath = path.join(__dirname, 'resources');
+        console.log(foldersPath);
+        */
+
+        const audioPath = 'C:\\Users\\Raymond\\Desktop\\Discord bots\\music-monkey\\resources\\monkey-bomb-song.mp3';
+        
+        const audioResource = createAudioResource(audioPath);
+        audioPlayer.play(audioResource);
+        
+        /*
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            console.log('Voice connection ready');
+        });
+        */
+
         await interaction.reply('Joining channel: ' + channel.name);
+
+        connection.subscribe(audioPlayer);
 
         await wait(2000);
         
         connection.destroy();
         await interaction.followUp('Disconnecting connection');
+
+        audioPlayer.stop();
 	},
 };
